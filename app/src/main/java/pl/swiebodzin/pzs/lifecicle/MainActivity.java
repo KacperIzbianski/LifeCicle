b12276517b430776d954bd29ff9c9be437fbf07f
maselo123456 <kacperizbianski@gmail.com>
2018-05-23 14:08:15 +0200

    lifecicle

--- app/src/main/java/pl/swiebodzin/pzs/lifecicle/MainActivity.java
+++ app/src/main/java/pl/swiebodzin/pzs/lifecicle/MainActivity.java
@@ -11,5 +11,42 @@ public class MainActivity extends AppCompatActivity {
         Log.d("life", "onCreate");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    @Override
+    protected void onStart() {
+        Log.d("life", "onStart");
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        Log.d("life" , "onResume");
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        Log.d("life" , "onPause");
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        Log.d("life" , "onStop");
+        super.onStop();
+    }
+
+    @Override
+    protected void onRestart() {
+        Log.d("life", "onRestart");
+        super.onRestart();
+    }
+
+    @Override
+    protected void onDestroy() {
+        Log.d("life" , "onDestroy");
+        super.onDestroy();
     }
 }
